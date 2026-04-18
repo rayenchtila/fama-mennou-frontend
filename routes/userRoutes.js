acbee2825bada7d53b0d0fcb535d2bf036203727
rayen ben chtila <rayen.benchtila@esprit.tn>
2026-04-19 00:22:52 +0100

    remove all backend files from frontend repo

--- routes/userRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { fetchUsers } from "../controllers/userController.js";
-
-const router = express.Router();
-
-router.get("/users", fetchUsers);
-
-export default router;
